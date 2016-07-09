525bc79d0e17f60ff995c88059f81e311a5edfd1
alon <alon.moscovitz@gmail.com>
2016-07-09 04:18:15 +0300

    feature2

--- root/src/main/java/com/alon/Main.java
+++ root/src/main/java/com/alon/Main.java
@@ -4,5 +4,6 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("Hello World maya!");
         System.out.println("Hello World! alon");
+        System.out.println("Hello feature2!");
     }
 }
