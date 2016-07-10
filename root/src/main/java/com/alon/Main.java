2e5c2848f0c4f7a5f7abecc31d2651957f8a1cbc
alon <alon.moscovitz@gmail.com>
2016-07-10 20:43:33 +0300

    feature2
    # Conflicts:
    #	root/src/main/java/com/alon/Main.java

--- root/src/main/java/com/alon/Main.java
+++ root/src/main/java/com/alon/Main.java
@@ -2,18 +2,6 @@ package com.alon;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello World maya!");
-        System.out.println("Hello World! alon");
-
-        System.out.println("Hello feature2!");
-        System.out.println("Hello feature2 + 2!");
-
-        System.out.println("Hello feature2 + 2 + 2!");
-        System.out.println("feature 1!");
-        System.out.println("feature 1 + 1!");
-        System.out.println("feature 1 + 1 + 1!");
-
-        System.out.println("Hello feature2!");
-        System.out.println("Hello feature2 + 2!");
+        // A
     }
 }
